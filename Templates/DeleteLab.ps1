82fed9bd3f34012567e448a48f7bf5a940677b33
Eric Cote <eric@coteexpert.com>
2017-04-12 19:29:17 -0400

    async args

--- Templates/DeleteLab.ps1
+++ Templates/DeleteLab.ps1
@@ -1,9 +1,11 @@
 $labs = Find-AzureRmResource -ResourceType 'Microsoft.DevTestLab/labs' -ResourceNameContains $labName
 $lab=$labs[0]
 
-$jobId = Start-Job -ScriptBlock {
+$jobId = Start-Job -ArgumentList $lab, $groupName -ScriptBlock {
   param($lab, $groupName)
   Remove-AzureRmResource -ResourceId $lab.ResourceId -Force
   #delete res group
   Remove-AzureRmResourceGroup -Name $groupName -Force
 }
+
+
