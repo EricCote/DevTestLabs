e24a330ee54b2177fe017a150f4c21fd62e56c8e
Eric Cote <eric@coteexpert.com>
2017-04-15 23:07:14 -0400

    add chrome customizations

---
+++ Artifacts/windows-customize-chrome/Customize-Chrome.ps1
@@ -0,0 +1,32 @@
+﻿$master_prefs = @'
+{ 
+ "browser": {
+   "default_browser_infobar_last_declined":"66666666666666",
+   "should_reset_check_default_browser": false,
+   "default_browser_setting_enabled": false
+             },
+ "first_run_tabs": ["about:newtab"],
+ "homepage" : "http://www.afiexpertise.com", 
+ "homepage_is_newtabpage" : false, 
+ "sync_promo" : {"show_on_first_run_allowed": false,  "user_skipped": true}, 
+ "distribution" : { 
+   "create_all_shortcuts" : false, 
+   "make_chrome_default" : false, 
+   "make_chrome_default_for_user": false, 
+   "suppress_first_run_default_browser_prompt": true,
+   "suppress_first_run_bubble": true, 
+   "do_not_create_desktop_shortcut":true,
+   "do_not_create_quick_launch_shortcut":true,
+   "do_not_create_taskbar_shortcut":true,
+   "do_not_create_any_shortcuts" : true,
+   "welcome_page_on_os_upgrade_enabled": false,
+   "msi":true,
+   "system_level":true,
+   "verbose_logging":true,
+   "allow_downgrade":false
+ }
+}
+'@
+
+Set-Content "c:\program files (x86)\Google\Chrome\Application\master_preferences" $master_prefs
+
