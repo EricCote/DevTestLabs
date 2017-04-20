8de2798d4ec43ea4da39e2ae7ecf8f45b52f603a
Eric Cote <eric@coteexpert.com>
2017-04-20 17:11:31 -0400

    corretion

--- Artifacts/windows-update-store-apps/Schedule-AppUpdate.ps1
+++ Artifacts/windows-update-store-apps/Schedule-AppUpdate.ps1
@@ -2,6 +2,8 @@
 New-PSDrive HKU Registry HKEY_USERS | out-null
 &reg load hku\def "C:\users\default user\NTUSER.DAT"
 
+New-item -path "hku:\def\Software\Microsoft\Windows\CurrentVersion\RunOnce" -force
+
 Set-ItemProperty  -path "hku:\def\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "update" -Value 'schtasks /run /tn "\Microsoft\Windows\WindowsUpdate\Automatic App Update"'
 
 [System.GC]::Collect();
