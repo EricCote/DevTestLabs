ddc47f68437085319ad30c608395ea71d846791c
Eric Cote <eric@coteexpert.com>
2017-04-21 00:02:45 -0400

    remove debug

--- Artifacts/windows-update-store-apps/Schedule-AppUpdate.ps1
+++ Artifacts/windows-update-store-apps/Schedule-AppUpdate.ps1
@@ -2,25 +2,18 @@
 New-PSDrive HKU Registry HKEY_USERS | out-null
 &reg load hku\def "C:\users\default user\NTUSER.DAT"
 
-"hive loaded"
 
-New-item -path "hku:\def\Software\Microsoft\Windows\CurrentVersion\RunOnce" -force
-"path created"
-
+New-item -path "hku:\def\Software\Microsoft\Windows\CurrentVersion\RunOnce" -force  | out-null
 Set-ItemProperty  -path "hku:\def\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "update" -Value 'schtasks /run /tn "\Microsoft\Windows\WindowsUpdate\Automatic App Update"'
-"update runonce with a new value"
+""
+
 
 [System.GC]::Collect();
-"first collect"
 [System.GC]::WaitForPendingFinalizers();
-"wait"
 [System.GC]::Collect();
-"second collect"
 
 &reg unload hku\def
-"hive unloaded"
-
 Remove-PSDrive HKU
-"Remove psdrive"
+
 
  
