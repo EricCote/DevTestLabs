50ff26439c942e6b395ea953939bb562930a29bf
Eric Cote <eric@coteexpert.com>
2017-04-21 19:02:01 -0400

    add profile back

--- Artifacts/windows-customize-edge/Customize-Edge.ps1
+++ Artifacts/windows-customize-edge/Customize-Edge.ps1
@@ -11,14 +11,17 @@ new-item "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Curre
 new-itemproperty "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\FirstRun" -Name "LastFirstRunVersionDelivered" -Value 1 -Type DWORD -Force | out-null ;
 new-itemproperty "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" -Name IE10TourShown -Value 1 -Type DWORD -Force | out-null;
 
+sc "$env:USERPROFILE\desktop\done.txt" (get-date)
+
 "@
 
 new-item "$env:ProgramData\scripts" -directory -force
 
 sc "$env:ProgramData\scripts\EdgeWelcome.ps1"  $script
 
+
 New-Item -Path   "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\EdgeWelcome" -type Directory 
 new-itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\EdgeWelcome" -Name "Version" -Value "1,0,0,0" -PropertyType String -Force 
-new-itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\EdgeWelcome" -Name "StubPath" -Value "powershell.exe  -NoProfile -ExecutionPolicy ByPass -File `"$env:ProgramData\scripts\EdgeWelcome.ps1`"" -PropertyType ExpandString -Force
+new-itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\EdgeWelcome" -Name "StubPath" -Value "powershell.exe  -ExecutionPolicy ByPass -File `"$env:ProgramData\scripts\EdgeWelcome.ps1`"" -PropertyType ExpandString -Force
 
 
