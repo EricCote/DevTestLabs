b94850766bbebf3a4c1b4b16fa509718c55dd427
Eric Cote <eric@coteexpert.com>
2020-11-26 23:40:14 -0500

    Update links for SQL 2020 express

--- Artifacts/windows-sql-localdb/Install-localdb.ps1
+++ Artifacts/windows-sql-localdb/Install-localdb.ps1
@@ -13,13 +13,20 @@ function Download-File
 }
 
 
-$sourceSqlLocalDb="https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SqlLocalDB.msi";
+$sourceSqlLocalDb2017="https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SqlLocalDB.msi";
+#V.2019
+$sourceSqlLocalDb="https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SqlLocalDB.msi";
+
 $SqlLocalDb=  "$env:temp\SqlLocalDB.msi"
 
+
 $sourceOdbc = "https://download.microsoft.com/download/E/6/B/E6BFDC7A-5BCD-4C51-9912-635646DA801E/msodbcsql_17.1.0.1_x64.msi"
+$sourceOdbc = "https://download.microsoft.com/download/6/b/3/6b3dd05c-678c-4e6b-b503-1d66e16ef23d/en-US/17.6.1.1/x64/msodbcsql.msi"
 $odbc = "$env:temp\msodbcsql.msi"
 
 $sourceCmdLineUtil = "https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/x64/MsSqlCmdLnUtils.msi"
+$sourceCmdLineUtil = "https://download.microsoft.com/download/0/e/6/0e63d835-3513-45a0-9cf0-0bc75fb4269e/EN/x64/MsSqlCmdLnUtils.msi"
+
 $CmdLineUtil = "$env:temp\MsSqlCmdLnUtils.msi"
 
 Download-File $sourceSqlLocalDb $SqlLocalDb
