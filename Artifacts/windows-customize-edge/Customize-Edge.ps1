97956b43e26b4bc9fe46a73e5eefe39123a759ea
Eric Côté <eric@coteexpert.com>
2021-05-23 01:52:16 -0400

    changess

--- Artifacts/windows-customize-edge/Customize-Edge.ps1
+++ Artifacts/windows-customize-edge/Customize-Edge.ps1
@@ -2,23 +2,23 @@
 
 
 $response = Invoke-WebRequest -Uri $edgeEnterpriseMSIUri -Method Get -ContentType "application/json" -UseBasicParsing -ErrorVariable InvokeWebRequestError
-$jsonObj = ConvertFrom-Json $([String]::new($response.Content))
+$jsonObj = ConvertFrom-Json $([String]::new($response.Content));
 
-$selectedIndex = [array]::indexof($jsonObj.Product, "Stable")
+$selectedIndex = [array]::indexof($jsonObj.Product, "Stable");
 
-$LatestEdgeUrl = ($jsonObj[$selectedIndex].Releases |
-            Where-Object { $_.Architecture -eq "x64" -and $_.Platform -eq "Windows"}  |
-        Sort-Object $_.ReleaseId -Descending)[0].Artifacts[0].Location
+$LatestEdgeUrl = ($jsonObj[$selectedIndex].Releases | `
+            Where-Object { $_.Architecture -eq "x64" -and $_.Platform -eq "Windows"}  | `
+        Sort-Object $_.ReleaseId -Descending)[0].Artifacts[0].Location;
 
 
 #download edge
-Invoke-WebRequest -Uri $LatestEdgeUrl -OutFile "$env:temp\edge.msi" -UseBasicParsing
+Invoke-WebRequest -Uri $LatestEdgeUrl -OutFile "$env:temp\edge.msi" -UseBasicParsing;
 
 #install edge
-msiexec /q /i "$env:temp\edge.msi"  ALLUSERS=1
+msiexec /q /i "$env:temp\edge.msi"  ALLUSERS=1;
 
 #hide first run popups
-New-ItemProperty -path "HKLM:\Software\Microsoft\Edge" -name "HideFirstRunExperience" -value 1
+New-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -name "HideFirstRunExperience" -value 1
     
 
 #Stop nagging default browser  
--- Artifacts/windows-customize-windows/Customize-Windows.ps1
+++ Artifacts/windows-customize-windows/Customize-Windows.ps1
@@ -1,4 +1,4 @@
-#Show hidden Files
+#Show file extensions
 Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced HideFileExt 0
 
 #Set eastern time zone
@@ -16,17 +16,17 @@ new-Item HKLM:\System\CurrentControlSet\Control\Network\NewNetworkWindowOff
 
 #Disable IE protection
 function Disable-IEESC{
-    $AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
-    $UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
-    Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
-    Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
 }
-Disable-IEESC
+Disable-IEESC;
 
 #disable ie fist run popups
 mkdir 'HKLM:\Software\Microsoft\Internet Explorer'
 mkdir 'HKLM:\Software\Microsoft\Internet Explorer\Main'
-New-ItemProperty -path "HKLM:\Software\Microsoft\Internet Explorer\Main" -name "DisableFirstRunCustomize" -value 1
+New-ItemProperty -path "HKLM:\Software\Microsoft\Internet Explorer\Main" -name "DisableFirstRunCustomize" -value 1;
 
 
 #disable server manager at login
