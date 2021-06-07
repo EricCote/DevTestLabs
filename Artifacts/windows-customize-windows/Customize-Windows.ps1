c7ed2f2898b1d5afd13c318a3e688a11a8394311
Eric Côté <eric@coteexpert.com>
2021-06-07 11:46:38 -0400

    fix

--- Artifacts/windows-customize-windows/Customize-Windows.ps1
+++ Artifacts/windows-customize-windows/Customize-Windows.ps1
@@ -40,22 +40,24 @@ New-ItemProperty -path "HKLM:\Software\Microsoft\Internet Explorer\Main" -name "
 #                  -name "PrivacyConsentStatus" `
 #                  -value 1
 
+mkdir 'HKLM:\Software\Microsoft\Windows\OOBE' -Force
 # disable "Choose Privacy Settings for your device"
 Set-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE" `
                  -name "DisablePrivacyExperience" `
                  -value 1
 
-Set-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE" `
-                 -name "SkipUserOOBE" `
-                 -value 1
-Set-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE" `
-                 -name "SkipMachineOOBE" `
-                 -value 1
+# Set-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE" `
+#                  -name "SkipUserOOBE" `
+#                  -value 1
+# Set-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE" `
+#                  -name "SkipMachineOOBE" `
+#                  -value 1
 
 # set taskbar
 mkdir c:\programData\script -Force
 Copy-Item ./taskbar.xml c:\programData\script\taskbar.xml
 
+mkdir 'HKLM:\Software\Microsoft\Windows\Explorer' -Force
 Set-ItemProperty -path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" `
                  -name "LockedStartLayout" `
                  -value 1
