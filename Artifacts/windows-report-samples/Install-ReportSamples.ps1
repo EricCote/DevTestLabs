b2c0f645383ef7a8cbe22a59d2a066af88c19e35
Eric Côté <eric@coteexpert.com>
2021-06-28 23:16:16 -0400

    changes

--- Artifacts/windows-report-samples/Install-ReportSamples.ps1
+++ Artifacts/windows-report-samples/Install-ReportSamples.ps1
@@ -1,7 +1,7 @@
-Set-Location c:\
+Set-Location c:\  | Out-File C:\wow\out.txt
 
-git clone https://github.com/EricCote/SSRS_Samples.git
+git clone https://github.com/EricCote/SSRS_Samples.git  | Out-File C:\wow\out.txt -append
 
-Set-Location c:\SSRS_Samples
+Set-Location c:\SSRS_Samples | Out-File C:\wow\out.txt -append
 
-& .\install-reports.ps1
+& .\install-reports.ps1 | Out-File C:\wow\out.txt -append
