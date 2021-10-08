331f3599f1ce744a62aea40f0157abc918015936
Eric Cote <eric@coteexpert>
2021-10-08 11:24:45 -0400

    latest fixes

--- Artifacts/windows-install-docker-win10/Install-Docker.ps1
+++ Artifacts/windows-install-docker-win10/Install-Docker.ps1
@@ -9,33 +9,14 @@ Invoke-WebRequest -uri "https://desktop.docker.com/win/stable/amd64/Docker%20Des
  
 & "$env:TEMP\installDocker.exe" install --quiet  | out-null
  
-
 while ((net localgroup) -inotcontains "*docker-users")
   {
       & ping 127.0.0.1 -n 10 
       out-file  "c:\programdata\script\result.txt" "Wait 10 "  -Append
   }
-& net localgroup docker-users afi /add *>&1 | out-file "c:\programdata\script\result.txt" -Append
-# & schtasks.exe /change  /tn DockerTask /disable
-#"@
+& net localgroup docker-users users /add *>&1 | out-file "c:\programdata\script\result.txt" -Append
 
-
-#mkdir -path $env:ProgramData\script -Force
-#$part2 | Out-File "$env:ProgramData\script\Docker.ps1"
-
-
-#& schtasks.exe /create /f /tn DockerTask /sc ONSTART /rl HIGHEST /RU "NT AUTHORITY\SYSTEM"  /tr "powershell.exe -ExecutionPolicy bypass -file c:\programdata\script\Docker.ps1"
-#restart-computer
-
-
-
-
-#install
-
-
-# refreshenv
 # Start-Service com.docker.service 
-
 # docker image pull alpine
 
 
