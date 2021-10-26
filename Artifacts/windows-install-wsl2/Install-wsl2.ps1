7a867af1712e0ca98e01b3043f4c95c516e0c72f
Eric Cote <eric@coteexpert>
2021-10-26 12:51:17 -0400

    changes

--- Artifacts/windows-install-wsl2/Install-wsl2.ps1
+++ Artifacts/windows-install-wsl2/Install-wsl2.ps1
@@ -2,6 +2,10 @@ $ProgressPreference = 'SilentlyContinue'
 
 Invoke-WebRequest -uri "https://aka.ms/wslubuntu2004" -UseBasicParsing -OutFile "$env:TEMP\Ubuntu.appx"
 Add-AppxProvisionedPackage -Online -SkipLicense -PackagePath "$env:TEMP\Ubuntu.appx"  -Regions all 
+Enable-WindowsOptionalFeature -FeatureName "Microsoft-Hyper-V-All"  -online  -norestart
+
+
+
 wsl --install
 restart-computer
 
