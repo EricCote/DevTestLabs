2d83c31303838506b1467a39edfed974681c844d
Eric Cote <eric@coteexpert>
2021-10-25 21:36:08 -0400

    changes

--- Artifacts/windows-vscode64/vscode.ps1
+++ Artifacts/windows-vscode64/vscode.ps1
@@ -1,10 +1,12 @@
+#increase download speed by hiding progress bar
 $ProgressPreference = 'SilentlyContinue'
 
-
 $setupExe = "$env:temp\VsCodeSetup.exe" 
-
 $url = 'https://update.code.visualstudio.com/latest/win32-x64/stable' 
-
 Invoke-WebRequest -Uri $url -OutFile $SetupExe -UseBasicParsing
 
-& "$setupExe" /123 /SP- /SUPPRESSMSGBOXES /VERYSILENT /NORESTART /MERGETASKS="!runcode" | out-null
+#call setup
+& "$setupExe" /SP- /SUPPRESSMSGBOXES /VERYSILENT /NORESTART /MERGETASKS="!runcode,addcontextmenufolders,addcontextmenufiles" | out-null
+
+#remove installation file
+Remove-Item -Path $SetupExe
