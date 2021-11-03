522e8ffbbba2af9e0580d638c1c7c7d4ff9fcaaa
Eric Cote <eric@coteexpert>
2021-11-03 03:17:32 -0400

    test!

--- Artifacts/windows-fr-ca-keyboard/Set-Langwin11.ps1
+++ Artifacts/windows-fr-ca-keyboard/Set-Langwin11.ps1
@@ -25,15 +25,15 @@ $lang = @(
     "Language.Handwriting~~~fr-FR~0.0.1.0",
     "Language.OCR~~~fr-CA~0.0.1.0",
     "Language.Speech~~~fr-CA~0.0.1.0",
-    "Language.TextToSpeech~~~fr-CA~0.0.1.0",
-    "Windows.Desktop.EMS-SAC.Tools~~~~0.0.1.0",
-    "Browser.InternetExplorer~~~~0.0.11.0",
-    "Microsoft.Windows.Notepad.System~~~~0.0.1.0",
-    "Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0",
-    "Print.Management.Console~~~~0.0.1.0",
-    "Print.Fax.Scan~~~~0.0.1.0",
-    "App.StepsRecorder~~~~0.0.1.0",
-    "Microsoft.Windows.WordPad~~~~0.0.1.0"
+    "Language.TextToSpeech~~~fr-CA~0.0.1.0"
+    # "Windows.Desktop.EMS-SAC.Tools~~~~0.0.1.0",
+    # "Browser.InternetExplorer~~~~0.0.11.0",
+    # "Microsoft.Windows.Notepad.System~~~~0.0.1.0",
+    # "Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0",
+    # "Print.Management.Console~~~~0.0.1.0",
+    # "Print.Fax.Scan~~~~0.0.1.0",
+    # "App.StepsRecorder~~~~0.0.1.0",
+    # "Microsoft.Windows.WordPad~~~~0.0.1.0"
 );
 
 $url="https://azureshelleric.blob.core.windows.net/win11/fr-ca/Microsoft-Windows-Client-Language-Pack_x64_fr-ca.cab?sp=r&st=2021-10-31T17:40:45Z&se=2023-11-01T01:40:45Z&spr=https&sv=2020-08-04&sr=b&sig=5xD%2BdyQwdlOSbqg%2FSW7IGHREGcdEJ%2Bc%2BMYNl5G6%2B6m4%3D"
@@ -46,7 +46,7 @@ Add-WindowsPackage -online -PackagePath  "$env:temp\lang.cab"
 
 
 
-$lang | ForEach-Object  { Remove-WindowsCapability -Online -Name $_  }
+$lang | ForEach-Object  { if ($_ -notmatch "Language\.")  {  Remove-WindowsCapability -Online -Name $_  }}
 "Removed capabilities" | out-file "$env:temp\wow3.txt"
 
 
