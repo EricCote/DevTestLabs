fda1655622d3c97ba0a779d653f581bcf4cd9c68
Eric Cote <eric@coteexpert>
2021-11-16 11:26:18 -0500

    update wsl2

--- Artifacts/windows-wsl2/Install-wsl2.ps1
+++ Artifacts/windows-wsl2/Install-wsl2.ps1
@@ -1,7 +1,14 @@
 $ProgressPreference = 'SilentlyContinue'
 
+Enable-WindowsOptionalFeature -FeatureName "VirtualMachinePlatform" -online -norestart
+
 Invoke-WebRequest -uri "https://aka.ms/wslubuntu2004" -UseBasicParsing -OutFile "$env:TEMP\Ubuntu.appx"
 Add-AppxProvisionedPackage -Online -SkipLicense -PackagePath "$env:TEMP\Ubuntu.appx"  -Regions all 
 
-wsl --install
+
+Invoke-WebRequest -uri "https://github.com/microsoft/WSL/releases/download/0.50.2/Microsoft.WSL_0.50.2.0_x64_ARM64.msixbundle" -UseBasicParsing -OutFile "$env:TEMP\wsl2.msixbundle"
+Add-AppxProvisionedPackage -Online -SkipLicense -PackagePath "$env:TEMP\wsl2.msixbundle"  -Regions all 
+
+
+#wsl --install
 restart-computer
