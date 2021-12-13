d917f36497c3e0e5b02ff31da1ce7883c7d282f3
Eric Cote <eric@coteexpert>
2021-12-12 21:13:34 -0500

    changes

--- Artifacts/windows-update-appx/Update-Appx.ps1
+++ Artifacts/windows-update-appx/Update-Appx.ps1
@@ -1,1 +1,7 @@
+$UserLanguageList = New-WinUserLanguageList -Language "en-US"
+$UserLanguageList.Add("fr-CA")
+Set-WinUserLanguageList -LanguageList $UserLanguageList -force
+
+
 & schtasks /run /tn \Microsoft\Windows\InstallService\ScanForUpdates
+
