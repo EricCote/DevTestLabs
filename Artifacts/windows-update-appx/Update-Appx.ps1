234da53d07593b2a9be8d3952d72aa481e7becd2
Eric Cote <eric@coteexpert>
2021-12-12 21:08:33 -0500

    changes

--- Artifacts/windows-update-appx/Update-Appx.ps1
+++ Artifacts/windows-update-appx/Update-Appx.ps1
@@ -1,7 +1,1 @@
-
-
-$namespaceName = "root\cimv2\mdm\dmmap"                                   
-$className = "MDM_EnterpriseModernAppManagement_AppManagement01"          
-$wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className       
-$result = $wmiObj.UpdateScanMethod() 
-$result                                     
+& schtasks /run /tn \Microsoft\Windows\InstallService\ScanForUpdates
