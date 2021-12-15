0b84345f06f7596deeb7f4bbf2bc6ed3a0f9eada
Eric Cote <eric@coteexpert>
2021-12-15 14:54:16 -0500

    changes

---
+++ Artifacts/windows-fr-ca-language-pt2/Set-Langwin11.ps1
@@ -0,0 +1,48 @@
+﻿
+$UserLanguageList = New-WinUserLanguageList -Language "fr-CA"
+$UserLanguageList.Add("en-US")
+
+Set-WinUserLanguageList -LanguageList $UserLanguageList -force
+
+#-------------------------------------------------------------------------
+
+$unattend = Get-Content "C:\windows\panther\Unattend.xml"
+
+$unattend = $unattend.Replace('<settings pass="oobeSystem" wasPassProcessed="true">',
+@"
+<settings pass="oobeSystem" wasPassProcessed="true">
+  <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <InputLocale>040c:0000040c;0809:00000809</InputLocale>
+    <SystemLocale>fr-CA</SystemLocale>
+    <UILanguage>fr-CA</UILanguage>
+    <UserLocale>fr-CA</UserLocale>
+    <UILanguageFallback>en-US</UILanguageFallback>
+  </component>
+"@
+)
+
+$unattend | Set-Content "C:\windows\panther\Unattend.xml"
+
+#--------------------------------------------------------------
+
+$oobe = @"
+<FirstExperience>
+  <oobe>
+    <defaults>
+      <language>3084</language>
+      <location>39</location>
+      <keyboard>0809:00000809</keyboard>
+      <timezone>Eastern Standard Time</timezone>
+      <adjustForDST>true</adjustForDST>
+    </defaults>
+  </oobe>
+</FirstExperience>
+"@
+
+New-Item -Path "C:\windows\System32\oobe" -Name "info" -ItemType "directory"
+
+$oobe | Set-Content "C:\windows\System32\oobe\info\oobe.xml"
+
+#--------------------------------------------------------------
+
+& C:\Windows\System32\oobe\oobeldr.exe /system
--- Artifacts/windows-fr-ca-language/Set-Langwin11.ps1
+++ Artifacts/windows-fr-ca-language/Set-Langwin11.ps1
@@ -72,57 +72,6 @@ $packages | ForEach-Object { Add-WindowsPackage -Online -PackagePath (join-path 
 $lang="fr-CA";
 $tempFolder="$env:temp"
 
-$UserLanguageList = New-WinUserLanguageList -Language "fr-CA"
-$UserLanguageList.Add("en-US")
-
-Set-WinUserLanguageList -LanguageList $UserLanguageList -force
-
-
-
-$unattend = Get-Content "C:\windows\panther\Unattend.xml"
-
-$unattend = $unattend.Replace('<settings pass="oobeSystem" wasPassProcessed="true">',
-@"
-<settings pass="oobeSystem" wasPassProcessed="true">
-  <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <InputLocale>040c:0000040c;0809:00000809</InputLocale>
-    <SystemLocale>fr-CA</SystemLocale>
-    <UILanguage>fr-CA</UILanguage>
-    <UserLocale>fr-CA</UserLocale>
-    <UILanguageFallback>en-US</UILanguageFallback>
-  </component>
-"@
-)
-
-
-
-$unattend | Set-Content "C:\windows\panther\Unattend.xml"
-
-$oobe = @"
-<FirstExperience>
-  <oobe>
-    <defaults>
-      <language>3084</language>
-      <location>39</location>
-      <keyboard>0809:00000809</keyboard>
-      <timezone>Eastern Standard Time</timezone>
-      <adjustForDST>true</adjustForDST>
-    </defaults>
-  </oobe>
-</FirstExperience>
-"@
-
-New-Item -Path "C:\windows\System32\msoobe" -Name "info" -ItemType "directory"
-
-$oobe | Set-Content "C:\windows\System32\msoobe\info\oobe.xml"
-
-
-
-
-
-
-& C:\Windows\System32\oobe\oobeldr.exe /system
-
 
 
 #Order the list accordingly 
@@ -329,8 +278,8 @@ function  ModifyRegistry($Regini, $profileName)
 # new-itemproperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name setToFrCA -Value "powershell -ExecutionPolicy bypass -WindowStyle hidden -File c:\programdata\script\frca.ps1"  -Force | out-null;
 
 
-# restart-computer
-# "Restarted"   | out-file "$env:temp\wow1.txt" -append
+restart-computer
+"Restarted"   | out-file "$env:temp\wow1.txt" -append
 
 #----------------------------------------------------------------
 
