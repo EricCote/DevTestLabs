5b53e4a725bbca3c15a7aee5e56c94ef4d38bc22
Eric Cote <eric@coteexpert>
2022-01-17 22:51:11 -0500

    test

--- Artifacts/windows-visualstudio/visualstudio.ps1
+++ Artifacts/windows-visualstudio/visualstudio.ps1
@@ -1,11 +1,15 @@
 [CmdletBinding()]
 Param ( 
-[string] $edition = "enterprise",
-[string] $vsVersion = "2022",
-[string]  $preview = "Release",
-[string] $workloads= "data,netweb",
+[string] $Edition = "Enterprise",
+[string] $VsVersion = "2022",
+[string] $Channel = "Release",
+[string] $Workloads= "Select",
+[string] $WorkloadList= "data,netweb",
 [string] $languages =  "en-US", 
-[string] $key = ""  )
+[string] $Key = "",
+[string] $Components = "",
+[string] $Restart = "No Restart"
+  )
 
 
 #Let's prepare a version of the key with no dashes.
@@ -16,17 +20,32 @@ $stringKey= if ($key) {"--productKey"} else {""};
 
 #Version number. 2017=15, 2019=16, 2022=17
 $ver=if ($vsVersion -eq "2017") {"15"} elseif ($vsVersion -eq "2019") {"16"} else {"17"}
-$prev =if($preview -eq 'Preview') {"pre"} else {"release"}
-$source = "https://aka.ms/vs/$ver/$prev/vs_$Edition.exe";
+
+$channl =if($Channel -eq 'Preview') {"pre"} else {"release"}
+$source = "https://aka.ms/vs/$ver/$channl/vs_$Edition.exe";
 
 
 $languageParams=$languages.Split(',') | where {$_ -ne "en-US"} | % { "--addProductLang $($_.Trim())" } 
 
-$prev =if($preview ) {"Preview"} else {"Release"}
 
-$channel="VisualStudio.$ver.$preview"
+# $channelVer="VisualStudio.$ver.$Channel"
 
-$loads = ($workloads -replace "\+", ";includeRecommended" -replace "\*",";includeOptional").split(",") | ? -Property Length -GT 0 | % { "--add  Microsoft.VisualStudio.Workload.$($_.trim())"}
+$LoadParams = switch ($Workloads) {
+    "All" { "--allWorkloads"  }
+    "All with Recommended" { "--allWorkloads --includeRecommended"  }
+    "All with Optional" { "--allWorkloads --includeOptional"  }
+    "All with Recommended and Optional" { "--allWorkloads --includeRecommended --includeOptional"  }
+    "Complete" { "--all"  }
+    Default { "" }
+}
+
+$loads = ($WorkloadList -replace "\+", ";includeRecommended" -replace "\*",";includeOptional").split(",") | ? -Property Length -GT 0 | % { "--add  Microsoft.VisualStudio.Workload.$($_.trim())"}
+$comps = $Components.split(",")  ? -Property Length -GT 0 | % { "--add $($_.trim())"}
+
+if ($LoadParams -ne "")
+{ $loads="" }
+
+$reboot = if ($restart -eq "No restart") {"--noRestart"} else {""}
 
 
 $dest = ( "${env:Temp}\vs_setup.exe");
@@ -40,13 +59,13 @@ catch
     Write-Error "Failed to download vs2022 installer";
 }
 
-$myParams =  " --productid Microsoft.VisualStudio.Product.$edition $loads $languageParams $stringKey $keyNoDashes --quiet  --wait  "
+$myParams =  " --productid Microsoft.VisualStudio.Product.$edition $loadParams $loads $comps $languageParams $stringKey $keyNoDashes $reboot --quiet  --wait  "
 $myParams2 = $myParams.Split(" ");
 
 
 try
 {      
-      & $dest $myParams2 | Out-Default;
+      & $dest $myParams2 | Out-Null;
 }
 catch
 {
@@ -54,6 +73,6 @@ catch
 }
 
 
-"& $dest $myParams"
+"& $dest $myParams2"
 
 
