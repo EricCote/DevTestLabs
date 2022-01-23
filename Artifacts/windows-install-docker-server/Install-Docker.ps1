676902182f986cd62d671ca54fe06de9a9b74dff
Eric Cote <eric@coteexpert>
2022-01-23 10:26:20 -0500

    docker server

--- Artifacts/windows-install-docker-server/Install-Docker.ps1
+++ Artifacts/windows-install-docker-server/Install-Docker.ps1
@@ -1,8 +1,10 @@
 #install
 $ProgressPreference = 'SilentlyContinue'
 
-Install-WindowsFeature -Name Hyper-V, Containers -IncludeAllSubFeature -IncludeManagementTools -restart
+Install-WindowsFeature -Name Hyper-V, Containers -IncludeAllSubFeature -IncludeManagementTools 
 
+# Set the TLS version used by the PowerShell client to TLS 1.2.
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 
 # Install-WindowsFeature -name Microsoft-Windows-Subsystem-Linux -restart
 #c:\tools\psexec -sid powershell
@@ -23,27 +25,27 @@ Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
 
 
-$configfile = @"
-{ "experimental": true }
-"@
-Out-File -FilePath C:\ProgramData\docker\config\daemon.json -InputObject $configfile -Encoding ascii -Force
+# $configfile = @"
+# { "experimental": true }
+# "@
+# Out-File -FilePath C:\ProgramData\docker\config\daemon.json -InputObject $configfile -Encoding ascii -Force
 
 
-Stop-service Docker -ErrorAction SilentlyContinue
+# Stop-service Docker -ErrorAction SilentlyContinue
 
 
-[Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "1", "Machine")
-[Environment]::SetEnvironmentVariable("LCOW_API_PLATFORM_IF_OMITTED", "linux", "Machine")
+# [Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "1", "Machine")
+# [Environment]::SetEnvironmentVariable("LCOW_API_PLATFORM_IF_OMITTED", "linux", "Machine")
  
 
-Invoke-WebRequest -Uri "https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip" -UseBasicParsing -OutFile "$env:temp\release.zip"
-Expand-Archive "$env:temp\release.zip" -DestinationPath "$Env:ProgramFiles\Linux Containers\."
+# Invoke-WebRequest -Uri "https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip" -UseBasicParsing -OutFile "$env:temp\release.zip"
+# Expand-Archive "$env:temp\release.zip" -DestinationPath "$Env:ProgramFiles\Linux Containers\."
 
-refreshenv
-Start-Service docker 
+# refreshenv
+# Start-Service docker 
 
-docker image pull mcr.microsoft.com/windows/servercore:ltsc2019
-docker image pull mcr.microsoft.com/windows/nanoserver:1809  
+docker image pull mcr.microsoft.com/windows/servercore:ltsc2022
+docker image pull mcr.microsoft.com/windows/nanoserver:ltsc2022  
 docker image pull mcr.microsoft.com/dotnet/core/aspnet
 docker image pull mcr.microsoft.com/dotnet/core/sdk
 
