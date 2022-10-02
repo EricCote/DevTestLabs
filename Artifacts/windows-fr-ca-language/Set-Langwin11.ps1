4f001feeb122a2159e1a416f952588dfb6c04bb4
Eric Cote <eric@coteexpert>
2022-10-02 19:48:02 -0400

    Add timings in logfile

--- Artifacts/windows-fr-ca-language/Set-Langwin11.ps1
+++ Artifacts/windows-fr-ca-language/Set-Langwin11.ps1
@@ -47,9 +47,9 @@ $url = "$blobLocation/Microsoft-Windows-Client-Language-Pack_x64_fr-ca.cab?$sas"
 
 
 Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile "$env:temp\lang.cab"
-"Lang Downloaded" | out-file  $logPath -append
+"Lang Downloaded $(Get-Date -Format T)"  | out-file  $logPath -append
 Add-WindowsPackage -online -PackagePath  "$env:temp\lang.cab"
-"Lang Installed" | out-file  $logPath -append
+"Lang Installed $(Get-Date -Format T)" | out-file  $logPath -append
 
 
 
@@ -59,36 +59,36 @@ Add-WindowsPackage -online -PackagePath  "$env:temp\lang.cab"
 
 
 $FOD | ForEach-Object { Add-WindowsCapability -Online -Name $_ }
-"Added FOD capabilities"  | out-file $logPath -append
+"Added FOD capabilities $(Get-Date -Format T)"  | out-file $logPath -append
 
 
 $packages = $linkArray | ForEach-Object { @{url = "$blobLocation/$($_)?$sas"; filename = $_ } }
-"generate a list of package names and url" | out-file $logPath -append
+"generate a list of package names and url $(Get-Date -Format T)" | out-file $logPath -append
 
 
 $packages | ForEach-Object { Invoke-WebRequest -UseBasicParsing -Uri $_.url -OutFile (join-path  $env:temp   $_.filename) } 
-"loop for download" | out-file $logPath -append
+"loop for download $(Get-Date -Format T)" | out-file $logPath -append
 
 
 $packages | ForEach-Object { Add-WindowsPackage -Online -PackagePath (join-path  $env:temp   $_.filename) }
-"loop for integrating windows package"  | out-file $logPath -append
+"loop for integrating windows package $(Get-Date -Format T)"  | out-file $logPath -append
 
 
 $url2 = "https://azureshelleric.blob.core.windows.net/win11-22h2/inbox-apps/inbox.zip?$sas"
 Invoke-WebRequest -UseBasicParsing -Uri $url2 -OutFile "$env:temp\inbox.zip"
-"Download inbox files"  | out-file $logPath -append
+"Download inbox files $(Get-Date -Format T)"  | out-file $logPath -append
 
 Expand-Archive -Path "$env:temp\inbox.zip" -DestinationPath "$env:temp\appx"   -Force
-"Unzip inbox files"  | out-file $logPath -append
+"Unzip inbox files $(Get-Date -Format T)"  | out-file $logPath -append
 
 foreach ($app in (Get-ChildItem $env:TEMP\appx\*.*xbundle )) {
-    $app.BaseName + " prep"  | out-file $logPath -append
+    $app.BaseName + " prep  $(Get-Date -Format T)"  | out-file $logPath -append
     $lic = "$($app.DirectoryName)\$($app.BaseName).xml"
-    Add-AppxProvisionedPackage -Online -PackagePath $($app.fullname) -LicensePath $lic  | out-file $logPath -append
-    $app.BaseName + " done"  | out-file $logPath -append
+    Add-AppxProvisionedPackage -Online -PackagePath $($app.fullname) -LicensePath $lic  | Out-Null
+    $app.BaseName + " done  $(Get-Date -Format T)"  | out-file $logPath -append
 }
 
 
 restart-computer
-"Restarted"   | out-file $logPath -append
+"Restarted  $(Get-Date -Format T)"   | out-file $logPath -append
 
