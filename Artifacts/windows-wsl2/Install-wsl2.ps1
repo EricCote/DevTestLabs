bdce624e94b40e89ddb0bdc44edd24974b0919cb
Eric Cote <eric@coteexpert>
2023-03-31 21:47:54 -0400

    changes

--- Artifacts/windows-wsl2/Install-wsl2.ps1
+++ Artifacts/windows-wsl2/Install-wsl2.ps1
@@ -1,4 +1,15 @@
 Enable-WindowsOptionalFeature -FeatureName "VirtualMachinePlatform" -online -norestart
+Enable-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux"  -online  -norestart
 
 
-wsl --install
+
+
+
+$latestSvc = "https://api.github.com/repos/microsoft/WSL/releases/latest";
+$response = Invoke-RestMethod -URI $latestSvc -UseBasicParsing
+$download_url = $response.assets[0].browser_download_url
+
+
+Invoke-WebRequest -uri $download_url -UseBasicParsing -OutFile "$env:TEMP\wsl2.msixbundle"
+Add-AppxProvisionedPackage -Online -SkipLicense -PackagePath "$env:TEMP\wsl2.msixbundle"  -Regions all 
+Add-AppxPackage -Online -SkipLicense -PackagePath "$env:TEMP\wsl2.msixbundle"  -Regions all 
