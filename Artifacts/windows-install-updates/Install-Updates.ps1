61de80b870e03c780f005851261597c3b1eb0389
Eric Cote <eric@coteexpert>
2023-04-01 18:46:01 -0400

    test

--- Artifacts/windows-install-updates/Install-Updates.ps1
+++ Artifacts/windows-install-updates/Install-Updates.ps1
@@ -1,9 +1,6 @@
 
 $ProgressPreference = 'SilentlyContinue'
 
-
-
-
 $sas = "sp=rl&st=2022-10-02T07:44:44Z&se=2026-10-02T15:44:44Z&spr=https&sv=2021-06-08&sr=c&sig=8COlEmuB7LVPphsQWBhfGPqx1guSF4MRWmRKdVU5Bvg%3D" 
 $blobLocation = "https://azureshelleric.blob.core.windows.net/win11-22h2";
 
@@ -11,7 +8,6 @@ $url = "$blobLocation/notepad.zip?$sas"
 
 
 Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile "$env:temp\notepad.zip"
-
 Expand-Archive -Path "$env:temp\notepad.zip" -DestinationPath "$env:temp\appx"   -Force
 
 
@@ -39,7 +35,7 @@ Remove-Item "C:\Windows\Temp\appx"  -Recurse -Force
 Install-PackageProvider -Name NuGet -force
 Install-Module PSWindowsUpdate -force
 
-Get-WUInstall  -AcceptAll -Install -AutoReboot
+Get-WUInstall  -AcceptAll -Install -IgnoreReboot
 
 #Install-WindowsUpdate -AcceptAll -verbose -IgnoreReboot
 
