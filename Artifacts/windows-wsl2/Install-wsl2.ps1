23c809a5c39706f2967e92619efa8b1ceb43d7f3
Eric Cote <eric@coteexpert>
2023-04-03 18:59:34 -0400

    test

--- Artifacts/windows-wsl2/Install-wsl2.ps1
+++ Artifacts/windows-wsl2/Install-wsl2.ps1
@@ -1,7 +1,7 @@
 $ProgressPreference = 'SilentlyContinue'
 
-Enable-WindowsOptionalFeature -FeatureName "VirtualMachinePlatform" -online -norestart
-Enable-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux"  -online  -norestart
+# Enable-WindowsOptionalFeature -FeatureName "VirtualMachinePlatform" -online -norestart
+# Enable-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux"  -online  -norestart
 
 
 #$latestSvc = "https://api.github.com/repos/microsoft/WSL/releases/latest";
@@ -17,32 +17,33 @@ Enable-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux"  
 # Invoke-WebRequest -UseBasicParsing -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile "$env:temp\wsl_update_x64.msi"  
 # & msiexec /i "$env:temp\wsl_update_x64.msi" /quiet 
 
+###
 
-$ProgressPreference = 'SilentlyContinue'
+# $ProgressPreference = 'SilentlyContinue'
 
-function install-appx($name, $id){
-  $postParams = @{type='ProductId';url=$id ;ring='Retail';lang='en-US'}
+# function install-appx($name, $id){
+#   $postParams = @{type='ProductId';url=$id ;ring='Retail';lang='en-US'}
   
-  $res = invoke-webrequest  -UseBasicParsing `
-     -Uri "https://store.rg-adguard.net/api/GetFiles" `
-     -ContentType "application/x-www-form-urlencoded" `
-     -Body $postParams `
-     -Method Post
+#   $res = invoke-webrequest  -UseBasicParsing `
+#      -Uri "https://store.rg-adguard.net/api/GetFiles" `
+#      -ContentType "application/x-www-form-urlencoded" `
+#      -Body $postParams `
+#      -Method Post
 
-  $link = $res.links[1].href
+#   $link = $res.links[1].href
 
-  Invoke-WebRequest -UseBasicParsing -uri $link  -OutFile "$env:temp\$name"
+#   Invoke-WebRequest -UseBasicParsing -uri $link  -OutFile "$env:temp\$name"
   
-  Add-AppxProvisionedPackage -SkipLicense -PackagePath "$env:temp\$name" -online
+#   Add-AppxProvisionedPackage -SkipLicense -PackagePath "$env:temp\$name" -online
 
-  Remove-Item "$env:temp\$name" -force
-}
+#   Remove-Item "$env:temp\$name" -force
+# }
 
-install-appx -name "wsl.msixbundle" -id "9P9TQF7MRM4R"
+# install-appx -name "wsl.msixbundle" -id "9P9TQF7MRM4R"
 
-install-appx -name "ubuntu.appxbundle" -id "9NBLGGH4MSV6"
+# install-appx -name "ubuntu.appxbundle" -id "9NBLGGH4MSV6"
 
-
+###
 
 $link="https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/updt/2022/03/wsl_update_x64_8b248da7042adb19e7c5100712ecb5e509b3ab5f.cab"
 Invoke-WebRequest -UseBasicParsing -uri $link  -OutFile "$env:temp\wslupdate.cab"
@@ -51,7 +52,7 @@ Invoke-WebRequest -UseBasicParsing -uri $link  -OutFile "$env:temp\wslupdate.cab
 
 
 
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss" -Name WslInstalling -Value "true"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss" -Name WslInstalling -Value 1
 & msiexec /i "$env:temp\wslupdate.msi" /quiet
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss" -Name WslInstalling
 
