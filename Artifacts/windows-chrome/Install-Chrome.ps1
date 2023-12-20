52ad91865bded15f7f2f34b788bf4bf528806cfd
Eric Cote <eric@coteexpert>
2023-12-20 13:17:25 -0500

    install chrome

---
+++ Artifacts/windows-chrome/Install-Chrome.ps1
@@ -0,0 +1,11 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = 'SilentlyContinue'
+
+$chromeEnterpriseMSIUri = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
+
+#download chrome
+Invoke-WebRequest -Uri $chromeEnterpriseMSIUri -OutFile "$env:temp\chrome.msi" -UseBasicParsing;
+
+#install chrome
+msiexec /q /i "$env:temp\chrome.msi"  ALLUSERS=1 | out-null
+
