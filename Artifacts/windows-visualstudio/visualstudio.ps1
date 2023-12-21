ff1f85427cc1602aed6807e0b05f6ecebea77ae5
Eric Cote <eric@coteexpert>
2023-12-21 00:15:17 -0500

    fix typo

--- Artifacts/windows-visualstudio/visualstudio.ps1
+++ Artifacts/windows-visualstudio/visualstudio.ps1
@@ -25,7 +25,7 @@ $channl =if($Channel -eq 'Preview') {"pre"} else {"release"}
 $source = "https://aka.ms/vs/$ver/$channl/vs_$Edition.exe";
 
 
-$languageParams=$languages.Split(',')  % { "--addProductLang $($_.Trim())" } 
+$languageParams=$languages.Split(',') | ForEach-Object { "--addProductLang $($_.Trim())" } 
 
 
 # $channelVer="VisualStudio.$ver.$Channel"
