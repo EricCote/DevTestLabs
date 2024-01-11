8b952056079ec5a427e8d32a8fe682405be51919
Eric Cote <eric@coteexpert>
2024-01-11 02:09:19 -0500

    script update-storeapps does not work

--- Artifacts/windows-update-store-apps/Update-StoreApps.ps1
+++ Artifacts/windows-update-store-apps/Update-StoreApps.ps1
@@ -1,3 +1,6 @@
+#does not work because https://github.com/microsoft/winget-cli/issues/2854
+
+
 $ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
     if ($ResolveWingetPath){
            $WingetPath = $ResolveWingetPath[-1].Path
@@ -11,13 +14,13 @@ cd  $WingetPath
 
 # ./winget settings --enable InstallerHashOverride
 
-./winget upgrade  
-       # --all `
-       # --silent `
-       # --accept-source-agreements `
-       # --accept-package-agreements `
-       # --verbose-logs `
-       # --force 
+./winget upgrade  `
+       --all `
+       --silent `
+       --accept-source-agreements `
+       --accept-package-agreements `
+       --verbose-logs `
+       --force 
 
     
 
@@ -31,10 +34,8 @@ cd  $WingetPath
        --silent `
        --accept-source-agreements `
        --accept-package-agreements `
-       --disable-interactivity `
        --verbose-logs `
-       --include-unknown `
        --force `
-       --scope machine
+
 "@
 
