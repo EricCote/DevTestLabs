bd078ace2f794ff6fad01732e6693c10b03ddfc7
Eric Cote <eric@coteexpert>
2024-01-27 22:43:39 -0500

    test stuff

--- Artifacts/windows-test/Test-Stuff.ps1
+++ Artifacts/windows-test/Test-Stuff.ps1
@@ -1,2 +1,30 @@
-$OSInfo = Get-WmiObject -Class Win32_OperatingSystem
-$OSInfo.MUILanguages
+[CmdletBinding()]
+param(
+    [ValidateSet("test1","test2")] 
+    [string] $MyString = 'test1',
+    [switch] $MyFlag
+)
+
+
+$MyString | Out-Default
+$MyFlag | Out-Default
+
+# report languages
+
+# $OSInfo = Get-WmiObject -Class Win32_OperatingSystem
+# $OSInfo.MUILanguages
+
+#----------------------------------------------------------
+
+
+# Error of 23h2 images: Security center is not properly shown
+# fix to install the proper version of security center:
+
+
+# $secLink='https://www.winhelponline.com/apps/SecurityHealthSetup.exe'
+# invoke-webrequest -UseBasicParsing  $secLink -OutFile $env:TEMP\securityhealthsetup.exe
+# & $env:TEMP\securityhealthsetup.exe
+
+#----------------------------------------------------------
+
+
