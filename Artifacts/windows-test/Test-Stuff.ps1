e9598630acd7d3782a92447db6a1a2b757d31d66
Eric Cote <eric@coteexpert.com>
2025-03-05 21:15:01 -0500

    test fixed

--- Artifacts/windows-test/Test-Stuff.ps1
+++ Artifacts/windows-test/Test-Stuff.ps1
@@ -24,7 +24,7 @@ $logPath = "$env:temp\log-sys-fr-ca.txt"
 
 $destination="$env:temp\lang"
 
-new-item -ItemType Directory -Path $destination -Force
+new-item -ItemType Directory -Path $destination -Force | Out-Null
 
 
 $url2 = "$blobLocation/inbox.zip?$sas"
@@ -34,23 +34,39 @@ Invoke-WebRequest -UseBasicParsing -Uri $url2 -OutFile "$destination\inbox.zip"
 Expand-Archive -Path "$destination\inbox.zip" -DestinationPath "$destination\appx"   -Force
 "Unzip inbox files $(Get-Date -Format T)"  | out-file $logPath -append
 
+ 
 foreach ($app in (Get-ChildItem $destination\appx\*.*xbundle )) {
     $app.BaseName + " prep  $(Get-Date -Format T)"  | out-file $logPath -append
     $lic = "$($app.DirectoryName)\$($app.BaseName).xml"
-    Add-AppxProvisionedPackage -Online -PackagePath $($app.fullname) -LicensePath $lic  | Out-Null
+    Add-AppxProvisionedPackage -Online -PackagePath $($app.fullname) -LicensePath $lic | out-file $logPath -append
     $app.BaseName + " done  $(Get-Date -Format T)"  | out-file $logPath -append
+    break
 }
 
 
 Remove-Item $destination  -Recurse -Force
-"Remove downloaded files"   | out-file $logPath -append
+"Remove downloaded files  $(Get-Date -Format T)" | out-file $logPath -append 
+
 
 
 
 #Install-Language -Language "fr-CA" -CopyToSettings
 #Set-SystemPreferredUILanguage "fr-CA"
 
-Restart-Computer -Force
+# Restart-Computer -Force
+
+
+# $progressPreference = 'silentlyContinue'
+# Write-Host "Installing WinGet PowerShell module from PSGallery..."
+# Install-PackageProvider -Name NuGet -Force | Out-Null
+# Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+# Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+# Repair-WinGetPackageManager
+# Write-Host "Done."
+
+
+
+#----------------------------------------------------------
 
 # report languages
 
