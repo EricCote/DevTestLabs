dba4a6840514fb2e1b4c67dbc7170b421888f1d6
Eric Cote <eric@coteexpert.com>
2025-03-08 22:31:35 -0500

    update test

--- Artifacts/windows-test/Test-Stuff.ps1
+++ Artifacts/windows-test/Test-Stuff.ps1
@@ -1,12 +1,17 @@
 [CmdletBinding()]
 param(
     [ValidateSet("test1", "test2")] 
-    [string] $MyString = 'test1',
+    [string] $test = 'test1',
     [switch] $MyFlag
 )
 
 
-$MyString | Out-Default
+if ($test -eq "test1") { test1 }
+if ($test -eq "test2") { test2 }
+if ($test -eq "test3") { test3 }
+if ($test -eq "test4") { test4 }
+
+
 $MyFlag | Out-Default
 
 
@@ -15,51 +20,62 @@ $MyFlag | Out-Default
 # dism /online /Get-ProvisioningPackageInfo
 
 
-Install-Language -Language "fr-CA" 
+function test1 {
+    Get-ChildItem "C:\program files\WindowsApps" 
 
-
-$ProgressPreference = 'SilentlyContinue'
-$Ver = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -name DisplayVersion 
-
-if ($Ver -eq '23H2') { $Ver = '22h2' } 
-
-$Ver = $ver.ToLower();
-
-$sas = "sv=2022-11-02&ss=b&srt=co&sp=rwdlaciytfx&se=2030-03-07T22:21:14Z&st=2025-03-07T14:21:14Z&spr=https&sig=xvYvvZHVuDVhQZdoal86XK35qlSEgQaTrFZC1qHPlLw%3D"
-$blobLocation = "https://azureshelleric.blob.core.windows.net/win11-$Ver/inbox-apps";
-
-$logPath = "$env:temp\log-sys-fr-ca.txt"
-
-
-$destination = "$env:temp\lang"
-
-new-item -ItemType Directory -Path $destination -Force | Out-Null
-
-
-$url2 = "$blobLocation/inbox.zip?$sas"
-Invoke-WebRequest -UseBasicParsing -Uri $url2 -OutFile "$destination\inbox.zip"
-"Download inbox files $(Get-Date -Format T)"  | tee -file $logPath -append
-
-Expand-Archive -Path "$destination\inbox.zip" -DestinationPath "$destination\appx"   -Force
-"Unzip inbox files $(Get-Date -Format T)"  | tee -file $logPath -append
-
- 
-foreach ($app in (Get-ChildItem $destination\appx\*.*xbundle )) {
-    $app.BaseName + " prep  $(Get-Date -Format T)"  | tee -file $logPath -append
-    $lic = "$($app.DirectoryName)\$($app.BaseName).xml"
-    Add-AppxProvisionedPackage -Online -PackagePath $($app.fullname) -LicensePath $lic *>&1 | tee -file $logPath -append
-    $app.BaseName + " done  $(Get-Date -Format T)"  | tee -file $logPath -append
 }
 
+function testx {
+    #Install-Language -Language "fr-CA" 
 
-Remove-Item $destination  -Recurse -Force
-"Remove downloaded files  $(Get-Date -Format T)" | out-file $logPath -append  
+
+    $ProgressPreference = 'SilentlyContinue'
+    $Ver = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -name DisplayVersion 
+
+    if ($Ver -eq '23H2') { $Ver = '22h2' } 
+
+    $Ver = $ver.ToLower();
+
+    $sas = "sv=2022-11-02&ss=b&srt=co&sp=rwdlaciytfx&se=2030-03-07T22:21:14Z&st=2025-03-07T14:21:14Z&spr=https&sig=xvYvvZHVuDVhQZdoal86XK35qlSEgQaTrFZC1qHPlLw%3D"
+    $blobLocation = "https://azureshelleric.blob.core.windows.net/win11-$Ver/inbox-apps";
+
+    $logPath = "$env:temp\log-sys-fr-ca.txt"
 
 
-<# Import-Module Appx
-Import-Module Dism
-Get-AppxPackage -AllUsers | Where PublisherId -eq 8wekyb3d8bbwe | Format-List -Property PackageFullName,PackageUserInformation
+    $destination = "$env:temp\lang"
+
+    new-item -ItemType Directory -Path $destination -Force | Out-Null
+
+
+    $url2 = "$blobLocation/inbox.zip?$sas"
+    Invoke-WebRequest -UseBasicParsing -Uri $url2 -OutFile "$destination\inbox.zip"
+    "Download inbox files $(Get-Date -Format T)"  | tee -file $logPath -append
+
+    Expand-Archive -Path "$destination\inbox.zip" -DestinationPath "$destination\appx"   -Force
+    "Unzip inbox files $(Get-Date -Format T)"  | tee -file $logPath -append
+
  
+    foreach ($app in (Get-ChildItem $destination\appx\*.*xbundle )) {
+        $app.BaseName + " prep  $(Get-Date -Format T)"  | tee -file $logPath -append
+        $lic = "$($app.DirectoryName)\$($app.BaseName).xml"
+        Add-AppxProvisionedPackage -Online -PackagePath $($app.fullname) -LicensePath $lic *>&1 | tee -file $logPath -append
+        $app.BaseName + " done  $(Get-Date -Format T)"  | tee -file $logPath -append
+    }
+
+
+    Remove-Item $destination  -Recurse -Force
+    "Remove downloaded files  $(Get-Date -Format T)" | tee -file $logPath -append  
+
+    Get-Content c:\windows\logs\dism\dism.log | Out-Default
+
+}
+
+function test2 {
+    Import-Module Appx
+    Import-Module Dism
+    Get-AppxPackage -AllUsers | Where PublisherId -eq 8wekyb3d8bbwe | Format-List -Property PackageFullName, PackageUserInformation
+}
+<# 
  #>
 
 
