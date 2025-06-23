c12611264c5db007c456cead4eb8a1b25bcd97b5
Eric Cote <eric@coteexpert.com>
2025-06-23 18:15:57 -0400

    dev drive changed for disk p:

--- Artifacts/windows-dev-drive/Dev-Drive.ps1
+++ Artifacts/windows-dev-drive/Dev-Drive.ps1
@@ -15,7 +15,7 @@ if ($IsVhd) {
     attach vdisk
     convert gpt
     create partition primary
-    assign letter=d
+    assign letter=p
     rem format quick fs=ntfs label=DevDrive
 "@ 
 
@@ -29,7 +29,7 @@ else {
     rem remove letter=d
     select disk 0
     create partition primary
-    assign letter=d 
+    assign letter=p
 "@
 
 }
@@ -38,22 +38,25 @@ else {
 & DiskPart /s $env:temp\dp.txt 
 & remove-item $env:temp\dp.txt
 
-& Format D: /v:DevDrive /DevDrv /Q /y 
-& fsutil devdrv trust /f D: 
+& Format p: /v:DevDrive /DevDrv /Q /y 
+& fsutil devdrv trust /f p: 
 
 
 #####
 
 
-New-Item -ItemType Directory -Path "d:\Packages" -Force | out-null 
-New-Item -ItemType Directory -Path "d:\Packages\npm" -Force | out-null
-New-Item -ItemType Directory -Path "d:\Packages\.nuget" -Force | out-null
-New-Item -ItemType Directory -Path "D:\Packages\.nuget\packages" -Force | out-null
-New-Item -ItemType Directory -Path "d:\Packages\vcpkg" -Force | out-null 
-New-Item -ItemType Directory -Path "d:\Packages\pip" -Force | out-null
-New-Item -ItemType Directory -Path "d:\Packages\cargo" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Packages" -Force | out-null 
+New-Item -ItemType Directory -Path "p:\Packages\npm" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Packages\.nuget" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Packages\.nuget\packages" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Packages\vcpkg" -Force | out-null 
+New-Item -ItemType Directory -Path "p:\Packages\pip" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Packages\cargo" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Packages\cargo" -Force | out-null
+New-Item -ItemType Directory -Path "p:\Projects" -Force | out-null
 
-New-Item -ItemType Directory -Path "d:\temp" -Force | out-null 
+
+New-Item -ItemType Directory -Path "p:\temp" -Force | out-null 
 
 & SETX /M npm_config_cache D:\Packages\npm
 & SETX /M NUGET_PACKAGES D:\Packages\.nuget\packages
@@ -77,13 +80,13 @@ if ($RemapTemp) {
     # use temp folder on dev drive
     New-ItemProperty -path "HKU:Default\Environment" `
         -name TEMP `
-        -Value 'D:\temp' -PropertyType string `
+        -Value 'p:\temp' -PropertyType string `
         -Force | out-null
 
     # use temp folder on dev drive                 
     New-ItemProperty -path "HKU:Default\Environment" `
         -name TMP `
-        -Value 'D:\temp' -PropertyType string `
+        -Value 'p:\temp' -PropertyType string `
         -Force | out-null     
           
     #for explanation: https://stackoverflow.com/questions/25438409/reg-unload-and-new-key
