b9f18e050a1b1544c5126fa84588e6ac012b954f
Eric Cote <eric@coteexpert.com>
2025-11-06 05:36:37 -0500

    fixes

--- Artifacts/windows-ssms/Remove-Ssms.ps1
+++ Artifacts/windows-ssms/Remove-Ssms.ps1
@@ -1,15 +1,14 @@
 ﻿
-function Uninstall-Program
-{
-    Param([parameter(Position=1)]
+function Uninstall-Program {
+    Param([parameter(Position = 1)]
         $Name,
         [switch] $List
     )
 
     $programs = (Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*) + `
-      (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*)  | `
-      Select-Object DisplayName, UninstallString | `
-      ? DisplayName -like $name ;
+    (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*)  | `
+        Select-Object DisplayName, UninstallString | `
+        ? DisplayName -like $name ;
 
     if ($programs -eq $null) {
         return "No programs found with the name: " + $name;
@@ -21,12 +20,12 @@ function Uninstall-Program
     else {
      
         $programs | `
-        % -Process {  $unstr=$_.UninstallString.Replace("\Package Cache\","\Package_Cache\" ).Replace("  "," ") ;
-                $items = ($unstr.split(" ",2));
-                $items[0]= $items[0].Replace("\Package_Cache\","\Package Cache\" ).Replace("`"", "") ;
-                $items[1]= $items[1].Replace("/I","/x");
-                "$($items[0]) $($items[1]) /quiet" 
-                & ($items[0]) $items[1] /quiet | Out-default;
+            % -Process { $unstr = $_.UninstallString.Replace("\Package Cache\", "\Package_Cache\" ).Replace("  ", " ") ;
+            $items = ($unstr.split(" ", 2));
+            $items[0] = $items[0].Replace("\Package_Cache\", "\Package Cache\" ).Replace("`"", "") ;
+            $items[1] = $items[1].Replace("/I", "/x");
+            "$($items[0]) $($items[1]) /quiet" 
+            & ($items[0]) $items[1] /quiet | Out-default;
         }
 
     }
@@ -77,3 +76,57 @@ uninstall-program "Microsoft Visual C++ 2015 Redistributable (x64)*"
 rd "C:\Program Files (x86)\Microsoft Visual Studio 14.0" -recurse -force
 
 
+#  <br />
+#       <dl>
+#       <dt>cs-CZ</dt>  
+#         <dd>Czech</dd>
+#       <dt>de-DE</dt>  
+#         <dd>German</dd>
+#       <dt>en-US</dt>  
+#         <dd>English</dd>
+#       <dt>es-ES</dt>  
+#         <dd>Spanish</dd>
+#       <dt>fr-FR</dt>  
+#         <dd>French</dd>
+#       <dt>it-IT</dt>  
+#         <dd>Italian</dd>
+#       <dt>ja-JP</dt>  
+#         <dd>Japanese</dd>
+#       <dt>ko-KR</dt>  
+#         <dd>Korean</dd>
+#       <dt>pl-PL</dt>  
+#         <dd>Polish</dd>
+#       <dt>pt-BR</dt>  
+#         <dd>Portuguese - Brazil</dd>
+#       <dt>ru-RU</dt>  
+#         <dd>Russian</dd>
+#       <dt>tr-TR</dt>  
+#         <dd>Turkish</dd>
+#       <dt>zh-CN</dt>  
+#         <dd>Chinese - Simplified</dd>
+#       <dt>zh-TW</dt>  
+#         <dd>Chinese - Traditional</dd>
+#       </dl>
+
+#  <br />Use a suffix to include additional components:
+#       <br /> 
+#       [+
+#       ] include recommended,
+#       [*
+#       ] include optional,
+#       [+*
+#       ] include recommended and optional 
+#       <br />  <br />
+#       Ex: AI+,BI+,HybridAndMigration+
+#       <br />  <br />
+#       <dl>
+#       <dt>AI</dt>  
+#         <dd>AI-powered assistants</dd>  
+#       <dt>BI</dt>  
+#         <dd>Business intelligence, Analytics, Integration, Reports</dd>  
+#       <dt>CodeTools</dt>  
+#         <dd>Tools to help with code (Git, Query Hint)</dd>  
+#       <dt>HybridAndMigration</dt>  
+#         <dd>Assess database upgrade readiness, and move your data with ease.</dd>  
+#       </dl>
+      
