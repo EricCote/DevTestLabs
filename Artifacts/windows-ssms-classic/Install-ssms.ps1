f223a93eff6b4d61d4148bde2f3486cadc489eb7
Eric Cote <eric@coteexpert.com>
2025-11-06 05:21:57 -0500

    update ssms

---
+++ Artifacts/windows-ssms-classic/Install-ssms.ps1
@@ -0,0 +1,23 @@
+﻿param (
+    [string]$lang = "English (409)"
+)
+
+$ProgressPreference = "SilentlyContinue"
+
+$Query = [regex]::Matches($lang, "\((.*)\)")
+$num = $Query.Groups[1].Value
+$langNumber="0x$num"
+$source="https://aka.ms/ssmsfullsetup?clcid=$langNumber"
+
+
+$destination = "$env:temp\ssms-setup.exe"
+
+Invoke-WebRequest  -UseBasicParsing -Uri $source  -OutFile $destination
+
+& $destination /install /quiet | out-default
+
+Remove-Item $destination
+
+
+
+
